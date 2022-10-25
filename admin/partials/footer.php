5d9212eb8e6173ad46289011185b207ef4e90678
Anna Laffoon <ree11194@gmail.com>
2022-10-25 13:39:04 -0400

    reusable footer module

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<div class="footer">
+    <div class="wrapper">
+        <p class="text-center">
+            2022 all rights reserved, developed by A Laffoon
+        </p>
+    </div>
+</div>
+</body>
+
+</html>
