5a44f81c5cc71722547646b972f802304b3e3e62
Anna Laffoon <ree11194@gmail.com>
2022-10-25 10:21:29 -0400

    admin page

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,68 @@
 <html>
-<title>Food Order Website - Home Page</title>
+
+<head>
+    <link rel="stylesheet" href="../css/admin.css">
+    <title>Food Order Website - Home Page</title>
+
+</head>
+
+<body>
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Admin Manager</a>
+                </li>
+                <li>
+                    <a href="#">Category</a>
+                </li>
+                <li>
+                    <a href="#">Food</a>
+                </li>
+                <li>
+                    <a href="#">Order</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>DASHBOARD</h1>
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br>
+                Categories
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br>
+                Categories
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br>
+                Categories
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br>
+                Categories
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+    <div class="footer">
+        <div class="wrapper">
+
+            <p class="text-center">
+                2022 all rights reserved, developed by A Laffoon
+            </p>
+        </div>
+    </div>
+</body>
+
 </html>
