c1d6fddacefc6b93ae01a30ca93b0f0f230d2e48
Anna Laffoon <ree11194@gmail.com>
2022-10-25 13:39:26 -0400

    restructured main admin page

--- admin/index.php
+++ admin/index.php
@@ -1,68 +1,31 @@
-<html>
+<?php include("partials/menu.php"); ?>
 
-<head>
-    <link rel="stylesheet" href="../css/admin.css">
-    <title>Food Order Website - Home Page</title>
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Dashboard</h1>
 
-</head>
-
-<body>
-    <div class="menu text-center">
-        <div class="wrapper">
-            <ul>
-                <li>
-                    <a href="#">Home</a>
-                </li>
-                <li>
-                    <a href="#">Admin Manager</a>
-                </li>
-                <li>
-                    <a href="#">Category</a>
-                </li>
-                <li>
-                    <a href="#">Food</a>
-                </li>
-                <li>
-                    <a href="#">Order</a>
-                </li>
-            </ul>
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br>
+            Categories
         </div>
-    </div>
-    <div class="main-content">
-        <div class="wrapper">
-            <h1>DASHBOARD</h1>
-
-            <div class="col-4 text-center">
-                <h1>5</h1>
-                <br>
-                Categories
-            </div>
-            <div class="col-4 text-center">
-                <h1>5</h1>
-                <br>
-                Categories
-            </div>
-            <div class="col-4 text-center">
-                <h1>5</h1>
-                <br>
-                Categories
-            </div>
-            <div class="col-4 text-center">
-                <h1>5</h1>
-                <br>
-                Categories
-            </div>
-            <div class="clearfix"></div>
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br>
+            Categories
         </div>
-    </div>
-    <div class="footer">
-        <div class="wrapper">
-
-            <p class="text-center">
-                2022 all rights reserved, developed by A Laffoon
-            </p>
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br>
+            Categories
         </div>
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br>
+            Categories
+        </div>
+        <div class="clearfix"></div>
     </div>
-</body>
+</div>
 
-</html>
+<?php include("partials/footer.php") ?>
